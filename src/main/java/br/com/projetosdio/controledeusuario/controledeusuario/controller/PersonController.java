f54c52f7595a20a347ad30c846e6a95dde40dcac
AntonioGomes42 <br.com.antoniogomes@gmail.com>
2021-09-06 17:23:53 -0300

    Updating README fle

--- src/main/java/br/com/projetosdio/controledeusuario/controledeusuario/controller/PersonController.java
+++ src/main/java/br/com/projetosdio/controledeusuario/controledeusuario/controller/PersonController.java
@@ -4,6 +4,7 @@ import br.com.projetosdio.controledeusuario.controledeusuario.dto.request.Person
 import br.com.projetosdio.controledeusuario.controledeusuario.dto.response.MessageResponseDTO;
 import br.com.projetosdio.controledeusuario.controledeusuario.exception.PersonNotFoundException;
 import br.com.projetosdio.controledeusuario.controledeusuario.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
--- src/main/java/br/com/projetosdio/controledeusuario/controledeusuario/service/PersonService.java
+++ src/main/java/br/com/projetosdio/controledeusuario/controledeusuario/service/PersonService.java
@@ -6,26 +6,20 @@ import br.com.projetosdio.controledeusuario.controledeusuario.entity.Person;
 import br.com.projetosdio.controledeusuario.controledeusuario.exception.PersonNotFoundException;
 import br.com.projetosdio.controledeusuario.controledeusuario.mapper.PersonMapper;
 import br.com.projetosdio.controledeusuario.controledeusuario.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
 
